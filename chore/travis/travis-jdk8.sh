c49982399d6d250a6ac24c06fb89cc89c87eec52
GluckZhang <zhanglong3030@qq.com>
2019-09-28 13:33:47 +0200

    test trusty ci environment

--- chore/travis/travis-jdk8.sh
+++ chore/travis/travis-jdk8.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -e
 
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
-
-wget archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
-unzip -qq apache-maven-3.3.9-bin.zip
-export M2_HOME=$PWD/apache-maven-3.3.9
-
 ./chore/travis/travis-test.sh
